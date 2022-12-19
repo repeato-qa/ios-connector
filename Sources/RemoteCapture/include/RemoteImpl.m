318e74d1422b0b9342ba67c71fbde426cb723ee3
Ahad Nawaz <Ahadnawaz58@gmail.com>
2022-12-19 08:17:27 +0500

    Resolved pod spec errors and warning

--- Sources/RemoteCapture/include/RemoteImpl.m
+++ Sources/RemoteCapture/include/RemoteImpl.m
@@ -15,12 +15,10 @@
 
 @implementation RemoteCapture(AutoConnect)
 + (void)load {
-    NSArray *arguments = [[NSProcessInfo processInfo] arguments];
-    //os_log(OS_LOG_DEFAULT, "%@: Launch arguments: %{public}@", self, arguments);
-    NSUInteger index = [arguments indexOfObject:@"host-address"];
     NSString *hostAddress = [[NSUserDefaults standardUserDefaults] stringForKey:@"host-address"];
     float scaleUpFactor = [[NSUserDefaults standardUserDefaults] floatForKey:@"scale-up-factor"];
-    NSString *model = [[UIDevice currentDevice] model];
+//    NSString *model = [[UIDevice currentDevice] model];
+
 #if TARGET_IPHONE_SIMULATOR
     // on simulators we fall back to localhost, since the DEVELOPER_HOST (Host.current().name) turned out to be slightly unreliable
     if (hostAddress == NULL) {
@@ -28,8 +26,13 @@
     }
 #endif
     if (hostAddress == NULL) {
+        #ifdef DEVELOPER_HOST
+        NSArray *arguments = [[NSProcessInfo processInfo] arguments];
+        //os_log(OS_LOG_DEFAULT, "%@: Launch arguments: %{public}@", self, arguments);
+        //    NSUInteger index = [arguments indexOfObject:@"host-address"];
         os_log(OS_LOG_DEFAULT, "%@: Host-address launch argument not found -> using fallback %{public}s! Launch arguments: %{public}@", self, DEVELOPER_HOST, arguments);
         [self startCapture:@DEVELOPER_HOST scaleUpFactor:scaleUpFactor];
+        #endif
     } else {
         //NSString *hostAddress = arguments[index + 1];
         os_log(OS_LOG_DEFAULT, "%@: Host-address: %{public}@!", self, hostAddress);
