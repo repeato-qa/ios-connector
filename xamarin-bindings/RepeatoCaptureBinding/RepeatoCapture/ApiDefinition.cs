4a635bb17c4f1936111552ca573ad9cab0951fa9
Stephan Petzl <stephan.petzl@gmail.com>
2023-06-05 17:17:03 +0200

    Add Xamarin bindings (#9)
    * added xamarin binding library
    
    * added test project
    
    * added instruction
    
    * change readme
    
    * fixed misspelling
    
    * fixed misspeling
    
    ---------
    
    Co-authored-by: ginger-777 <7snyou2@i.ua>

---
+++ xamarin-bindings/RepeatoCaptureBinding/RepeatoCapture/ApiDefinition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using ObjCRuntime;
+using Foundation;
+using UIKit;
+
+
+namespace RepeatoCapture
+{
+    [BaseType(typeof(NSObject))]
+    public interface RepeatoCapture
+    {
+        [Static]
+        [Export("load")]
+        void Load();
+    }
+
+    [BaseType(typeof(NSObject))]
+    public interface InfoMessages
+    {
+        [Export("showAlert")]
+        void ShowAlert();
+
+        [Export("noLaunchArgumentsPassed")]
+        void NoLaunchArgumentsPassed();
+
+    }
+
+    [BaseType(typeof(NSObject))]
+    public interface Logger
+    {
+     
+    }
+
+}
+
+
