87f506176ef32b931ea3e05d61e268675c1af699
Stephan Petzl <stephan.petzl@gmail.com>
2025-02-24 11:25:48 +0100

    Support new line repeato command for textviews and textfields (#12)
    * New line repeato command handled for textview and textfield
    
    * Small change in protocol
    
    ---------
    
    Co-authored-by: Muhammad Ahad <ahadnawaz58@gmail.com>

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -13,6 +13,43 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        performAction()
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func performAction() {
+        print("onEnd editing|Done with text filed editing")
+    }
+}
+
+
+extension ViewController: UITextViewDelegate {
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        print("onEnd editing|Done with text view editing")
+    }
+}
+
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("onEnd editing|searchBarSearchButtonClicked")
+    }
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        print("onEnd editing|searchBarShouldEndEditing")
+        return true
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        print("onEnd editing|searchBarTextDidEndEditing")
+    }
+}
--- Sources/RepeatoCapture/include/RepeatoCapture.h
+++ Sources/RepeatoCapture/include/RepeatoCapture.h
@@ -574,7 +574,7 @@ static CGSize bufferSize; // current size of off-screen image buffers
     *(float *)device.remote.scale = scaleUpFactor;
     *(int *)device.remote.isIPad = [UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad;
     
-    *(int *)device.remote.protocolVersion = 127;
+    *(int *)device.remote.protocolVersion = 128;
     CGRect screenBounds = [self screenBounds];
     CGSize screenSize = screenBounds.size;
 
@@ -828,7 +828,15 @@ static int frameno; // count of frames captured and transmmitted
             fakeEvent->_timestamp = timestamp;
 
             if (sentText) {
-                if ([sentText isEqualToString:@"repeato:clear_text"]){
+                if([sentText isEqualToString:@"repeato:enter"]){
+                    if ([textField isKindOfClass:UITextView.class]) {
+                        [textField insertText:@"\n"];
+                    }
+                    else if ([textField.delegate respondsToSelector:@selector(textFieldShouldReturn:)]) {
+                        [textField.delegate textFieldShouldReturn:textField];
+                    }
+                }
+                else if ([sentText isEqualToString:@"repeato:clear_text"]){
                     Log(self, @"Remote asked to clear text");
                     [textField setText:@""];
                 }
